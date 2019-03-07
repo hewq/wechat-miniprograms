a9db060d7f2ae4e06cfb76dbc5818e7bfa4d3c14
wenqiu.he <wenqiu.he@yahoo.com>
2019-03-07 18:18:05 +0800

    bilibili 首页导航模块

--- bilibili/pages/index/index.js
+++ bilibili/pages/index/index.js
@@ -4,14 +4,45 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    // 被点击的首页导航菜单索引
+    currentIndexNav: 0,
+    // 首页导航数据
+    navList: []
+  },
+
+  /**
+   * 点击首页导航
+   */
+  activeNav (e) {
+    this.setData({
+      currentIndexNav: e.target.dataset.index
+    })
+  },
+
+  /**
+   * 获取首页导航数据
+   */
+  getNavList () {
+    let that = this;
+    // 利用小程序内置发送请求的方法
+    wx.request({
+      url: 'https://easy-mock.com/mock/5c1dfd98e8bfa547414a5278/bili/navList',
+      success (res) {
+        if (res.data.code === 0) {
+          that.setData({
+            navList: res.data.data.navList
+          })
+        }
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    // 获取首页导航数据
+    this.getNavList();
   },
 
   /**
